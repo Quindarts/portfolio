2d65eb5e9f8d23a8b60b2c1b27d127e2e988f9c8
Quindarts <quang82thcspb@gmail.com>
2024-02-22 12:40:22 +0700

    ver1

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -20,21 +20,21 @@ function page() {
                     </h2>
                     <div className="my-7 h-44 rounded-md bg-amber-100 p-7 ">
                         <h1 className="mb-3 text-gray-600">2021 - Present </h1>{' '}
-                        <h1 className="text-xl font-[600]">
+                        <h1 className="text-md font-[600]">
                             Student in The Industrial University of Ho Chi Minh City (IUH){' '}
                         </h1>
                         <h1 className="mt-4 text-gray-700">Go Vap, Nguyen Van Bao, HCM City</h1>
                     </div>
                     <div className="my-7 h-44 rounded-md bg-amber-100 p-7">
                         <h1 className="mb-3 text-gray-600">8/2022 - 6/2023 </h1>{' '}
-                        <h1 className="text-xl font-[600]">
+                        <h1 className="text-md font-[600]">
                             Achieved a 70% scholarship majoring in software engineering
                         </h1>
                         <h1 className="mt-4 text-gray-700">Semester 5, IUH School</h1>
                     </div>
                     <div className="my-7 h-44 rounded-md bg-amber-100 p-7">
                         <h1 className="mb-3 text-gray-600">4/2023 - 6/2023 </h1>{' '}
-                        <h1 className="text-xl font-[600]">Champion of WEBDEV ADVENTURE 2023</h1>
+                        <h1 className="text-md font-[600]">Champion of WEBDEV ADVENTURE 2023</h1>
                         <h1 className="mt-4 text-gray-700">WebDev Studios, VNUHCM - UIT</h1>
                     </div>
                 </div>
@@ -45,17 +45,17 @@ function page() {
                     </h2>
                     <div className="my-7 h-44 rounded-md bg-blue-100 p-5">
                         <h1 className="mb-3 text-gray-600">5/2022 - 7/2022 </h1>{' '}
-                        <h1 className="text-xl font-[600]">Freelance Frontend Developer JS/Bootstrap </h1>
+                        <h1 className="text-md font-[600]">Freelance Frontend Developer JS/Bootstrap </h1>
                         <h1 className="mt-4 text-gray-700">Vinaole - Phu My Huong, Q.7, HCM City</h1>
                     </div>
                     <div className="my-7 h-44 rounded-md bg-blue-100 p-5">
                         <h1 className="mb-3 text-gray-600">9/2022 - 6/2023 </h1>{' '}
-                        <h1 className="text-xl font-[600]">Frontend Developer ReactJs/TypeScripts </h1>
+                        <h1 className="text-md font-[600]">Frontend Developer ReactJs/TypeScripts </h1>
                         <h1 className="mt-4 text-gray-700">Build a ecommerce app Accessories</h1>
                     </div>
                     <div className="my-7 h-44 rounded-md bg-blue-100 p-5">
                         <h1 className="mb-3 text-gray-600">9/2023 - 12/2023 </h1>
-                        <h1 className="text-xl font-[600]">Technical Web collaborator in CodeMely</h1>
+                        <h1 className="text-md font-[600]">Technical Web collaborator in CodeMely</h1>
                         <h1 className="mt-4 text-gray-700">Gen 0.5</h1>
                     </div>
                 </div>
